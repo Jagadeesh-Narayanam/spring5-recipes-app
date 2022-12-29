6055ec442f5d4d28b6193fafa017a5000cd8cfca
Jagadeesh-Narayanam <jagadeesh.narayanam@valuemedi.com>
2022-12-29 17:34:51 +0530

    data initialization using spring

---
+++ src/main/java/com/springframework/spring5recipesapp/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5recipesapp.repositories;
+
+import com.springframework.spring5recipesapp.domain.Category;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoryRepository extends CrudRepository<Category,Long> {
+}
---
+++ src/main/java/com/springframework/spring5recipesapp/repositories/UnitOfMeasureRepository.java
@@ -0,0 +1,7 @@
+package com.springframework.spring5recipesapp.repositories;
+
+import com.springframework.spring5recipesapp.domain.UnitOfMeasure;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure,Long> {
+}
